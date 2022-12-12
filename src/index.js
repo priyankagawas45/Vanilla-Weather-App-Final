254b7e5c586c23d5a93911c12445fbe3f6843b54
priyanka Gawas <priyankasalunkhe45@gmail.com>
2022-12-12 20:54:58 +0100

    Added API Key and date time

--- src/index.js
+++ src/index.js
@@ -1,1 +1,122 @@
-//challenge 1 date and time
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}, ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        ` <div class="col-2">
+        <div class="weather-forecast-day">${formatDay(forecastDay.time)}</div>
+        <div class="weather-forecast-icon">
+            <img
+                src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+                  forecastDay.condition.icon
+                }.png"
+                alt="Snow"
+                width="40"
+                />
+            </div>
+        <div class="weather-forecast-temperature">
+        <span class="temperature-max">${Math.round(
+          forecastDay.temperature.maximum
+        )}º</span>
+        <span class="temperature-min">${Math.round(
+          forecastDay.temperature.minimum
+        )}º</span>
+        </div>
+    </div>`;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "ae183e1c92o08fb1071d0e97f254bdtd";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayWeatherCondition(response) {
+  let cityElement = document.querySelector("#city");
+  let countryElement = document.querySelector("#country");
+  let temperatureElement = document.querySelector("#temperature");
+  let realFeelElement = document.querySelector("#real-feel");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let nowElement = document.querySelector("#now");
+  let weatherIconElement = document.querySelector("#weather-icon");
+
+  celsiusTemperature = response.data.temperature.current;
+
+  cityElement.innerHTML = response.data.city;
+  countryElement.innerHTML = response.data.country;
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  descriptionElement.innerHTML = response.data.condition.description;
+  realFeelElement.innerHTML = Math.round(response.data.temperature.feels_like);
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  nowElement.innerHTML = formatDate(response.data.time * 1000);
+  weatherIconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  weatherIconElement.setAttribute("alt", response.data.condition.icon);
+
+  getForecast(response.data.coordinates);
+}
+
+function search(city) {
+  let apiKey = "ae183e1c92o08fb1071d0e97f254bdtd";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("Mannheim");
+displayForecast();
